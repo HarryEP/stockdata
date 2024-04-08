440982232105014914056b0abe6fa249a14b467d
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-04-08 19:30:07 +0100

    updated requirements

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+from datetime import date
 import yfinance as yf
 import numpy as np
 import streamlit as st
 import pandas as pd
 
+TODAY = date.today().strftime("%Y-%m-%d")
 
-def load_data(stock: str) -> pd.DataFrame:
+
+def load_data(stock: str, start: date, end: date) -> pd.DataFrame:
     '''load the data to a dataframe to use within the code'''
-    data = yf.download(stock, "2023-01-01", "2024-01-01")
+    data = yf.download(stock, start, end)
     data.reset_index(inplace=True)
     return data
 
 
-def retrieve_dates():
-    '''function to get what dates to use'''
-    pass
-
-
 def main():
     '''function to run everything'''
-    st.write(load_data('KO').tail())
+    start_date = st.sidebar.date_input('start')
+    end_date = st.sidebar.date_input('end')
+    st.write(load_data('KO', start_date, end_date).tail())
 
 
 if __name__ == "__main__":
