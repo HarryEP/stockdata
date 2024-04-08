495d6939ce6be0d7dc5dbacb0effa2a119c622f1
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-04-08 01:09:24 +0100

    making cleaner functions and a skeleton

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ import pandas as pd
 
 
 def load_data(stock: str) -> pd.DataFrame:
+    '''load the data to a dataframe to use within the code'''
     data = yf.download(stock, "2023-01-01", "2024-01-01")
     data.reset_index(inplace=True)
     return data
 
 
-if __name__ == "__main__":
+def retrieve_dates():
+    '''function to get what dates to use'''
+    pass
+
+
+def main():
+    '''function to run everything'''
     st.write(load_data('KO').tail())
+
+
+if __name__ == "__main__":
+
+    main()
