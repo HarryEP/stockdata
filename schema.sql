4091a3f33579d67ecb9b72aee4649c65260af30c
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-04-13 23:51:26 +0100

    added database to be storing the data into

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS stockinformation;
+CREATE DATABASE stockinformation;
+
+\c  stockinformation;
+
+CREATE SCHEMA pricing;
+
+SET search_path TO pricing;
+
+CREATE TABLE IF NOT EXISTS company (
+    company_id INT GENERATED ALWAYS AS IDENTITY,
+    symbol TEXT NOT NULL UNIQUE,
+    company_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS prices(
+    price_id INT GENERATED ALWAYS AS IDENTITY,
+    company_id INT NOT NULL,
+    price_date TIMESTAMP NOT NULL,
+    open_price REAL,
+    high REAL,
+    low REAL,
+    close_price REAL,
+    adj_close_price REAL,
+    volume INT,
+    FOREIGN KEY (company_id) REFERENCES company(company_id)
+);
