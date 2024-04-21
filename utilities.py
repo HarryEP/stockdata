69c937122b192a1ad42ad726bc203beb6bbb3444
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-04-21 16:24:20 +0100

    restructuring code

--- utilities.py
+++ utilities.py
@@ -11,5 +11,5 @@ def get_company_name(symbol: str) -> str:
         return stock_info["longName"]
     except Exception as e:
         print(f'{e} Error occured')
-        stock_name = input("Enter stock name manually: ")
-        return stock_name
+    stock_name = input("Enter stock name manually: ")
+    return stock_name
