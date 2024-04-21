082b03dcef4b59496101b3ef2911fcf6c2b5d006
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-04-21 21:11:23 +0100

    adding sql commands to retrieve needed data

--- analysis.py
+++ analysis.py
@@ -8,17 +8,27 @@ from psycopg2.extensions import connection
 from load import get_connection
 
 
-def choose_companies(conn: connection, schema_name):
+def choose_companies(conn: connection, schema_name: str) -> list[str]:
     with conn.cursor() as cur:
         cur.execute("SET search_path TO %s", (schema_name,))
         cur.execute("SELECT company_id, symbol FROM company")
         companies = cur.fetchall()
         # [RealDictRow([('company_id', 1), ('symbol', 'KO')]), RealDictRow([('company_id', 2), ('symbol', 'AAPL')]), RealDictRow([('company_id', 3), ('symbol', 'MSFT')])]
-        symbols = []
-        for company in companies:
-            symbols.append(company['symbol'])
-        print(symbols)
-    st.sidebar.multiselect('Select companies: ', symbols)
+        symbols = [company['symbol'] for company in companies]
+    selected_companies = st.sidebar.multiselect('Select companies: ', symbols)
+    return selected_companies
+
+
+def retrieve_data(conn: connection):
+    '''function to use SQL to get all the required data'''
+    with conn.cursor() as cur:
+        cur.execute('''SELECT c.symbol, p.price_date, p.open_price, p.high, p.low,
+                    p.close_price, p.adj_close_price, p.volume
+                    FROM prices p
+                    JOIN company c ON p.company_id=c.company_id
+                    ''')
+        data = cur.fetchall()
+        print(data)
 
 
 def main():
@@ -26,7 +36,8 @@ def main():
     load_dotenv()
     new_conn = get_connection(os.environ["DB_HOST"], os.environ["DB_NAME"],
                               os.environ["DB_PASS"], os.environ["DB_USER"])
-    choose_companies(new_conn, os.environ["SCHEMA"])
+    companies = choose_companies(new_conn, os.environ["SCHEMA"])
+    st.write(retrieve_data(new_conn))
 
 
 if __name__ == "__main__":
