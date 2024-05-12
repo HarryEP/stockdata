5af3dfb2b74440092a62c868b1adb3397b7a58e7
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-05-12 15:30:52 +0100

    adding temporary index

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+
+@app.route("/")
+def index():
+    return "Use this program to find out about stock data", 200
